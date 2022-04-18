109c4521eff5e6208669e71e333476df2ad05932
elamre <elmarvanrijnswou@hotmail.com>
2022-04-18 13:26:31 +0200

    First working version of client lobby and server

--- net_systems/lobby_system/lobby_client.go
+++ net_systems/lobby_system/lobby_client.go
@@ -1,26 +1,34 @@
 package lobby_system
 
 import (
+	"fmt"
 	"github.com/elamre/gomberman/net"
 	"github.com/elamre/gomberman/net/packet_interface"
-	common_system2 "github.com/elamre/gomberman/net_systems/common_system"
+	"github.com/elamre/gomberman/net_systems/common_system"
 	"github.com/elamre/gomberman/net_systems/common_system/common_packets"
-	packets2 "github.com/elamre/gomberman/net_systems/lobby_system/lobby_system_packets"
+	"github.com/elamre/gomberman/net_systems/lobby_system/common"
+	. "github.com/elamre/gomberman/net_systems/lobby_system/lobby_system_packets"
 	"log"
 )
 
+type LobbyCallback func(success bool, reason string, player *common_system.NetPlayer)
+
 type LobbyClientSystem struct {
-	OnRegisteredAction func()
-	curPlayer          *common_system2.NetPlayer
+	OnRegisteredAction LobbyCallback
+	OnRoomCreate       LobbyCallback
+	OnRoomJoin         LobbyCallback
+	OnRoomStart        LobbyCallback
+	curPlayer          *common_system.NetPlayer
 	client             net.Client
+	curRoom            *common.NetRoom
 }
 
 func NewLobbyClientSystem(client net.Client) *LobbyClientSystem {
-	c := &LobbyClientSystem{client: client, curPlayer: &common_system2.NetPlayer{}}
+	c := &LobbyClientSystem{client: client, curPlayer: &common_system.NetPlayer{}}
 	return c
 }
 
-func (c *LobbyClientSystem) SendRoomPacket(pack packets2.RoomPacket) any {
+func (c *LobbyClientSystem) SendRoomPacket(pack RoomPacket) any {
 	pack.UserId = c.curPlayer.Id
 	return c.client.WritePacket(pack)
 
@@ -40,23 +48,25 @@ func (c *LobbyClientSystem) RegisterPlayer(name string) {
 	}
 }
 
-func (p *LobbyClientSystem) connectionCallback(c net.Client, d common_system2.ClientRegulator, pack packet_interface.Packet) {
+func (p *LobbyClientSystem) connectionCallback(c net.Client, d common_system.ClientRegulator, pack packet_interface.Packet) {
 	t := pack.(common_packets.ConnectionPacket)
 	log.Printf("Client received: %s", t.String())
 	if t.Action == common_packets.ConnectionRefusedAction {
 		p.curPlayer.HasRegistered = false
+		if p.OnRegisteredAction != nil {
+			p.OnRegisteredAction(false, t.Message, p.curPlayer)
+		}
 		log.Printf("Unable to register: %s", t.Message)
 	} else if t.Action == common_packets.ConnectionAcceptedAction {
 		p.curPlayer.Id = t.UserId
 		if p.OnRegisteredAction != nil {
-			p.OnRegisteredAction()
+			p.OnRegisteredAction(true, t.Message, p.curPlayer)
 		}
-		log.Println("accepted")
 	} else {
 		log.Printf("Player with name: %s registered (%d)", t.Message, t.UserId)
 	}
 }
-func (lp *LobbyClientSystem) roomupdateCallback(c net.Client, d common_system2.ClientRegulator, pack packet_interface.Packet) {
+func (lp *LobbyClientSystem) roomupdateCallback(c net.Client, d common_system.ClientRegulator, pack packet_interface.Packet) {
 	/*	t := pack.(packets2.RoomUpdatePacket)
 		for _, room := range t.Rooms {
 			log.Printf("Room: %s", room.RoomName)
@@ -68,14 +78,22 @@ func (lp *LobbyClientSystem) roomupdateCallback(c net.Client, d common_system2.C
 		}*/
 }
 
-func (lp *LobbyClientSystem) roomPacketCallback(c net.Client, d common_system2.ClientRegulator, pack packet_interface.Packet) {
-	log.Printf("Received: %+v", pack)
+func (lp *LobbyClientSystem) roomPacketCallback(c net.Client, d common_system.ClientRegulator, pack packet_interface.Packet) {
+	t := pack.(RoomPacket)
+	if t.Action == RoomJoinSuccessAction || t.Action == RoomCreateSuccessAction {
+		lp.curRoom = &common.NetRoom{
+			RoomName: t.Name,
+			Owner:    t.UserId,
+			Players:  nil,
+		}
+	}
+	log.Printf("Received: %+v", t)
 }
 
-func (p *LobbyClientSystem) RegisterCallbacks(r common_system2.ClientRegulator) {
+func (p *LobbyClientSystem) RegisterCallbacks(r common_system.ClientRegulator) {
 	r.RegisterPacketCallback(p.connectionCallback, common_packets.ConnectionPacket{})
-	r.RegisterPacketCallback(p.roomupdateCallback, packets2.RoomUpdatePacket{})
-	r.RegisterPacketCallback(p.roomPacketCallback, packets2.RoomPacket{})
+	r.RegisterPacketCallback(p.roomupdateCallback, RoomUpdatePacket{})
+	r.RegisterPacketCallback(p.roomPacketCallback, RoomPacket{})
 }
 
 func (c *LobbyClientSystem) Update() {
@@ -83,4 +101,41 @@ func (c *LobbyClientSystem) Update() {
 		return
 	}
 	// We can draw here something
+}
+
+func (c *LobbyClientSystem) CreateRoom(roomName string, password string) any {
+	if !c.client.IsConnected() {
+		return fmt.Errorf("not connected")
+	}
+	c.SendRoomPacket(RoomPacket{
+		UserId:   c.curPlayer.Id,
+		Action:   RoomCreateAction,
+		Password: password,
+		Name:     roomName,
+	})
+	return nil
+}
+
+func (c *LobbyClientSystem) StartRoom() any {
+	if !c.client.IsConnected() {
+		return fmt.Errorf("not connected")
+	}
+	c.SendRoomPacket(RoomPacket{
+		UserId: c.curPlayer.Id,
+		Action: RoomStartAction,
+		Name:   c.curRoom.RoomName,
+	})
+	return nil
+}
+
+func (c *LobbyClientSystem) SetReady() any {
+	if !c.client.IsConnected() {
+		return fmt.Errorf("not connected")
+	}
+	c.SendRoomPacket(RoomPacket{
+		UserId: c.curPlayer.Id,
+		Action: RoomReadyAction,
+		Name:   c.curRoom.RoomName,
+	})
+	return nil
 }
--- net_systems/lobby_system/lobby_server.go
+++ net_systems/lobby_system/lobby_server.go
@@ -63,24 +63,10 @@ func (s *LobbyServerSystem) ClientDisconnect(c common_system2.ServerPlayer) {
 func (s *LobbyServerSystem) roomPacketCallback(c *common_system2.ServerPlayer, d common_system2.ServerRegulator, pack packet_interface.Packet) {
 	t := pack.(packets2.RoomPacket)
 	log.Printf("Server room: %s", t.String())
-	name := strings.TrimSpace(strings.ToLower(t.Name))
-	room := s.nameToRoom[name]
-	switch t.Action {
-	case packets2.RoomReadyAction:
-		// Check for illegal action
-		log.Printf("%+v", s.nameToRoom)
-		log.Printf("%+v", s.nameToRoom[name])
-		user := s.nameToRoom[name].Players[t.UserId]
-		s.nameToRoom[name].Players[t.UserId].Ready = !user.Ready
-	case packets2.RoomStartAction:
-		if room.IsReady() {
-			if s.OnRoomStart != nil {
-				s.OnRoomStart(room)
-			}
-		} else {
-			c.WritePacket(common_packets.ChatPacket{Message: "Can't start while not everybody is ready"})
-		}
-	case packets2.RoomCreateAction:
+
+	if t.Action == packets2.RoomCreateAction {
+		name := strings.TrimSpace(strings.ToLower(t.Name))
+
 		for _, r := range s.rooms {
 			if strings.Compare(r.RoomName, name) == 0 {
 				c.WritePacket(packets2.RoomPacket{
@@ -102,10 +88,40 @@ func (s *LobbyServerSystem) roomPacketCallback(c *common_system2.ServerPlayer, d
 			Action: packets2.RoomCreateSuccessAction,
 			Name:   name,
 		})
-	case packets2.RoomLeaveAction:
-		if room.Owner == c.NetPlayer.Id {
-			delete(s.nameToRoom, room.RoomName)
-			// Delete the room
+	} else {
+		room := s.nameToRoom[c.NetPlayer.RoomId]
+		switch t.Action {
+		case packets2.RoomReadyAction:
+			// Check for illegal action
+			c.NetPlayer.Ready = !c.NetPlayer.Ready
+			log.Printf("%+v", s.nameToRoom[c.NetPlayer.RoomId].Players)
+			for _, p := range room.Players {
+				s.userManage.IdToClient[p.Id].WritePacket(packets2.RoomPacket{
+					UserId: room.Owner,
+					Action: packets2.RoomStartAction,
+					Name:   room.RoomName,
+				})
+			}
+		case packets2.RoomStartAction:
+			if room.IsReady() {
+				for _, p := range room.Players {
+					s.userManage.IdToClient[p.Id].WritePacket(packets2.RoomPacket{
+						UserId: room.Owner,
+						Action: packets2.RoomStartAction,
+						Name:   room.RoomName,
+					})
+				}
+				if s.OnRoomStart != nil {
+					s.OnRoomStart(room)
+				}
+			} else {
+				c.WritePacket(common_packets.ChatPacket{Message: "Can't start while not everybody is ready"})
+			}
+		case packets2.RoomLeaveAction:
+			if room.Owner == c.NetPlayer.Id {
+				delete(s.nameToRoom, room.RoomName)
+				// Delete the room
+			}
 		}
 	}
 }
