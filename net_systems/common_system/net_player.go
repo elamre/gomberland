54f0ec236bca752ec987ab7de0d0e67eaf4493a0
elamre <elmarvanrijnswou@hotmail.com>
2022-04-18 13:26:07 +0200

    Adding string method

--- net_systems/common_system/net_player.go
+++ net_systems/common_system/net_player.go
@@ -1,10 +1,20 @@
 package common_system
 
-import "github.com/elamre/gomberman/net"
+import (
+	"fmt"
+	"github.com/elamre/gomberman/net"
+)
 
 type ServerPlayer struct {
 	NetPlayer *NetPlayer
 	Client    net.ServerClient
+}
+
+func (s ServerPlayer) String() string {
+	if s.NetPlayer == nil {
+		return fmt.Sprintf("%+v nil", s.Client)
+	}
+	return fmt.Sprintf("%+v, %s", s.NetPlayer, s.NetPlayer.String())
 }
 
 type NetPlayer struct {
@@ -13,4 +23,18 @@ type NetPlayer struct {
 	Name          string
 	Id            uint32
 	RoomId        string
+}
+
+func (n NetPlayer) String() string {
+	retString := fmt.Sprintf("%s[%d]", n.Name, n.Id)
+	if !n.HasRegistered {
+		retString += " [not registered]"
+	}
+	if n.Ready {
+		retString += " [ready]"
+	}
+	if n.RoomId != "" {
+		retString += fmt.Sprintf(" [room: %s]", n.RoomId)
+	}
+	return retString
 }
